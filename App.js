95947abbc520c3b7d05c061953c88c2847c673cb
Larry Wen <l.wen@griffith.edu.au>
2024-04-11 17:38:11 +1000

    - first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState, useEffect } from "react";
+import { loadData, saveData } from "./src/datamodel/mydata";
 export default function App() {
+  const [name, setName] = useState("");
+  useEffect(() => {
+    const fun = async () => {
+      const data = await loadData();
+      setName(data?.name);
+    };
+    fun();
+  }, []);
+  const nameChangeHandler = (val) => setName(val);
+  const submitHandler = () => saveData({ name });
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <TextInput
+        placeholder="Input Name"
+        value={name}
+        onChangeText={nameChangeHandler}
+        onSubmitEditing={submitHandler}
+        style={styles.textInput}
+      />
+      <Button title="submit" onPress={submitHandler} />
     </View>
   );
 }
@@ -13,8 +30,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    width: 150,
+    padding: 5,
   },
 });
